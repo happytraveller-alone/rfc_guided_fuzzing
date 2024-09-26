d980433ef96655a55a2d7afd7b91d3e1924a7aa0
happytraveller-alone <happytraveller@163.com>
2024-09-26 21:14:58 +0800

    revert: :rewind: sync files

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # RustDataProcess
-rust code process reverse engine code and rfc file.
 
-具体细节参看各个文件夹下的README.md
+RustDataProcess is a data processing tool written in Rust, designed to efficiently handle and manipulate large datasets.
+
+## Features
+
+- Fast and efficient data processing for reverse engineering code and RFC file.
+- Process the reverse engineering code to get the function name, and insert LLM prompt to get the function summary.
+- Process the RFC file to get the RFC summary.
+- Match the function summary and RFC summary to get the final summary.
--- rfc_processor/src/main.rs
+++ rfc_processor/src/main.rs
@@ -25,6 +25,7 @@ use colored::*;
 /// 作者：yuanfeng xie
 /// 日期：2024/07/29
 fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
+    // 获取 RFC 编号，如果没有提供则使用默认值
     let rfc_number = if args.len() != 2 {
         eprintln!("{}", "Warning: No RFC number provided.".red());
         println!("Please provide an RFC number. Using default RFC 2675.");
@@ -34,11 +35,13 @@ fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
         args[1].clone()
     };
 
+    // 获取项目根目录并创建输入输出目录
     let project_root = utils::get_project_root();
     let (input_dir, output_dir) = utils::create_directories(&project_root)?;
     let input_file = input_dir.join(format!("{}.txt", rfc_number));
     println!("Starting to process RFC {}", rfc_number);
 
+    // 如果本地文件不存在，则下载 RFC 文件
     if !input_file.exists() {
         println!("Downloading RFC {}", rfc_number);
         download::download_rfc(&rfc_number, &input_file)?;
@@ -46,26 +49,32 @@ fn run(args: Vec<String>) -> Result<(), Box<dyn Error>> {
         println!("Found local file {:?}", input_file);
     }
 
+    // 处理 RFC 内容
     println!("Processing RFC content");
     let body = process::process_rfc_content(&input_file)?;
 
+    // 创建输出目录
     println!("Creating output directory");
     let rfc_output_dir = utils::create_rfc_output_directory(&output_dir, &rfc_number)?;
 
+    // 获取 RFC 标题并保存信息
     println!("Getting RFC title");
     let rfc_title = download::get_rfc_title(&rfc_number)?;
     utils::save_rfc_info(&rfc_number, &rfc_title, &rfc_output_dir)?;
 
+    // 移除页眉和页脚
     println!("Removing headers and footers");
     let pre_processed_content = utils::remove_headers_footers(&input_file)?;
     let pre_processed_file = rfc_output_dir.join("pre_processed.txt");
     utils::save_content(&pre_processed_content, &pre_processed_file)?;
     println!("Pre-processed content saved to {:?}", pre_processed_file);
 
+    // 保存处理后的内容
     println!("Saving processed content");
     let body_file = rfc_output_dir.join(format!("{}_processed.txt", rfc_number));
     utils::save_content(&body, &body_file)?;
 
+    // 切片处理内容
     println!("Slicing content");
     slice::slice_content(&body, &rfc_output_dir, &rfc_number)?;
 
